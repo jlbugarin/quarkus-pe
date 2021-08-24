9acee5deb3dbe561348d7142369a60e99bc814ef
jlbugarin <jlbugarin@gmail.com>
2021-08-23 19:19:16 -0500

    Update MainApp.java

--- quarkus-pe/src/main/java/com/consultorjava/quarkus/MainApp.java
+++ quarkus-pe/src/main/java/com/consultorjava/quarkus/MainApp.java
@@ -1,5 +1,6 @@
 package com.consultorjava.quarkus;
 
+import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
@@ -7,6 +8,7 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Quarkus.run(RunHolaResource.class, args);
 
 	}
 
